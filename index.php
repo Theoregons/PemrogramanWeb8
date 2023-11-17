6a36a273a76d8f31612f52ad99e3871d6bb222fd
Baktiar Mahardika <baktiarmahardika08@gmail.com>
2023-11-17 15:53:42 +0700

    #perintahTugas5

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php 
-    $nama = "Baktiar";
+    // $nama = "Baktiar";
 
-    echo "Hallo, Selemat Datang $nama </br>";
-    echo 'Hallo, Selemat Datang $nama';
+    // echo "Hallo, Selamat Datang $nama </br>";
+    // echo 'Hallo, Selamat Datang $nama';
+
+    $x = 10;
+    $y = 20;
+    echo $x + $y . "</br>";
+    echo $x - $y . "</br>";
+    echo $x * $y . "</br>";
+    echo $x / $y . "</br>";
+    echo $x % $y . "</br>";
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
