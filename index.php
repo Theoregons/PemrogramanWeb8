b06ade05b15354b82a37338acc7fb324199272ce
Baktiar Mahardika <baktiarmahardika08@gmail.com>
2023-11-17 15:58:58 +0700

    #perintahTugas7

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <?php 
-    $nama_depan = "Baktiar";
-    $nama_belakang = "Mahardika";
+    $x = 10;
+    $x += 2;
+    echo $x . "</br>";
 
-    echo $nama_depan . " " . $nama_belakang;
-  
+    $y = 10;
+    $y -= 2;
+    echo $y . "</br>";
+
+    $z = 10;
+    $z /= 2;
+    echo $z . "</br>";
+    
+    $a = 10;
+    $a %= 2;
+    echo $a . "</br>";
+    
+    $b = 10;
+    $b .= 2;
+    echo $b . "</br>";
 ?>
 <!DOCTYPE html>
 <html lang="en">
